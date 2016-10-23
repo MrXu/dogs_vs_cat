b7e48cd1c26b2a7c84c90cc51ac0cb74864dd568
xuwei <lawist2010@gmail.com>
2016-10-23 23:47:20 +0800

    Add: predict

--- naive_convnet.py
+++ naive_convnet.py
@@ -8,6 +8,8 @@ from keras.utils.visualize_util import plot
 
 tf.python.control_flow_ops = tf
 
+# input: (150, 150, 3) matrix - 150*150 image with RGB 
+# output: 1/0
 def get_model():
 	model = Sequential()
 	model.add(Convolution2D(32, 3, 3, input_shape=(150, 150, 3)))
@@ -37,42 +39,63 @@ def get_model():
 
 	return model
 
-model = get_model()
+def train():
+	model = get_model()
 
-# this is the augmentation configuration we will use for training
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+	# this is the augmentation configuration we will use for training
+	train_datagen = ImageDataGenerator(
+	        rescale=1./255,
+	        shear_range=0.2,
+	        zoom_range=0.2,
+	        horizontal_flip=True)
 
-# this is the augmentation configuration we will use for testing:
-# only rescaling
-test_datagen = ImageDataGenerator(rescale=1./255)
+	# this is the augmentation configuration we will use for testing:
+	# only rescaling
+	test_datagen = ImageDataGenerator(rescale=1./255)
 
-# this is a generator that will read pictures found in
-# subfolers of 'data/train', and indefinitely generate
-# batches of augmented image data
-train_generator = train_datagen.flow_from_directory(
-        'data/train',  # this is the target directory
-        target_size=(150, 150),  # all images will be resized to 150x150
-        batch_size=32,
-        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+	# this is a generator that will read pictures found in
+	# subfolers of 'data/train', and indefinitely generate
+	# batches of augmented image data
+	train_generator = train_datagen.flow_from_directory(
+	        'data/train',  # this is the target directory
+	        target_size=(150, 150),  # all images will be resized to 150x150
+	        batch_size=32,
+	        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
 
-# this is a similar generator, for validation data
-validation_generator = test_datagen.flow_from_directory(
-        'data/validation',
-        target_size=(150, 150),
-        batch_size=32,
-        class_mode='binary')
+	# this is a similar generator, for validation data
+	validation_generator = test_datagen.flow_from_directory(
+	        'data/validation',
+	        target_size=(150, 150),
+	        batch_size=32,
+	        class_mode='binary')
 
 
-model.fit_generator(
-        train_generator,
-        samples_per_epoch=1999,
-        nb_epoch=20,
-        validation_data=validation_generator,
-        nb_val_samples=800)
-model.save_weights('first_try.h5')  # always save your weights after training or during training
+	model.fit_generator(
+	        train_generator,
+	        samples_per_epoch=1999,
+	        nb_epoch=20,
+	        validation_data=validation_generator,
+	        nb_val_samples=800)
 
-plot(model, to_file='model.png')
+	## post processing
+	# 1. save weights
+	model.save_weights('model.h5')  # always save your weights after training or during training
+	# 2. plot model
+	plot(model, to_file='model.png')
+	# 3. save model as json
+	model_json = model.to_json()
+	with open("model.json", "w") as json_file:
+		json_file.write(model_json)
+
+
+
+if __name__=="__main__":
+	img_width, img_height = 150, 150
+	train_data_dir = 'data/train'
+	validation_data_dir = 'data/validation'
+	nb_train_samples = 2000
+	nb_validation_samples = 800
+	nb_epoch = 50
+
+
+	train()
--- predict.py
+++ predict.py
@@ -1,13 +1,28 @@
 from naive_convnet import get_model
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
-def predict():
+def predict(img_path):
+
 	print "Loading model and weights ... "
 	model = get_model()
 	model.load_weights("model.h5")
 	print "Loaded"
 
+	print "compilling model"
+	model.compile(loss='binary_crossentropy',
+	              optimizer='rmsprop',
+	              metrics=['accuracy'])
+
+	img = load_img(img_path, target_size=(150, 150))
+	# this is a Numpy array with shape (3, 150, 150)
+	x = img_to_array(img)  
+	x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+	print "loaded image of size: "+str(x.shape)
+	res = model.predict_proba(x)
+	print "result: "+str(res)
 
 
-if __name__ == "main":
-	predict()
+if __name__=="__main__":
+	img_path = 'data/train/cats/cat.0.jpg'
+	predict(img_path)
 
